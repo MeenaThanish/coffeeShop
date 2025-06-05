418c71286e1d9fc4bc6600e597829396c6b74d98
MeenaThanish <meensmca@gmail.com>
2025-06-05 15:55:35 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,11 @@
 // add delayed functionality here
+// Select all elements that have a 'data-id' attribute
+document.querySelectorAll('[data-id]').forEach(element => {
+    // Get the value of 'data-id'
+    let dataIdValue = element.getAttribute('data-id');
+    
+    // Set the 'id' attribute with the same value
+    element.setAttribute('id', dataIdValue);
+});
+
+console.log("ID attributes added based on data-id values.");
