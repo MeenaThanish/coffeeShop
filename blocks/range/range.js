5baad653d868669b47d4fc6b6394b1f5d270379a
MeenaThanish <meensmca@gmail.com>
2025-09-26 15:51:05 +0530

    Update range.js

--- blocks/range/range.js
+++ blocks/range/range.js
@@ -1,3 +1,9 @@
-function updateLabel(value) {
-  document.getElementById('rangeValue').textContent = value;
-}
+window.onload = function () {
+    const slider = document.getElementById('rangeSlider');
+    const valueDisplay = document.getElementById('rangeValue');
+
+    // Update the displayed value when the slider changes
+    slider.addEventListener('input', function () {
+        valueDisplay.textContent = slider.value;
+    });
+};
