bb27faea8cdf7f4acfa6ade4b26cccb370ccd432
MeenaThanish <meensmca@gmail.com>
2025-10-03 17:56:40 +0530

    Update timeline.js

--- blocks/timeline/timeline.js
+++ blocks/timeline/timeline.js
@@ -1,13 +1,15 @@
-
+<script>
   window.onload = function () {
     var timeline = document.querySelector(".timeline");
     if (!timeline) return;
 
-    var childDivs = timeline.children;
+    var childDivs = timeline.querySelectorAll("div");
 
     for (var i = 0; i < childDivs.length; i++) {
-      if (i % 2 === 0) { // odd in human terms: 1st, 3rd, 5th...
+      // Check if the div is a direct child of .timeline
+      if (childDivs[i].parentElement === timeline && i % 2 === 0) {
         childDivs[i].classList.add("container", "left-container");
       }
     }
   };
+</script>
